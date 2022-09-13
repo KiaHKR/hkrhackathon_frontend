7332c33ac9e3893e0742de01e6fa21a830db2df5
Lasse Poulsen <work.lasse.face@gmail.com>
2022-09-13 22:23:33 +0200

    updated backend url

--- src/globals.ts
+++ src/globals.ts
@@ -1,2 +1,2 @@
 // export const BASE_API_URL: string = 'https://hkr-hackathon.herokuapp.com'
-export const BASE_API_URL: string = 'http://localhost:3000';
+export const BASE_API_URL: string = 'www.lekatehdas.net/hackathon/';
